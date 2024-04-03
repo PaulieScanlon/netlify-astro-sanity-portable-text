099bbfe76c202090aa62e384791bce4e29085e4e
Paul Scanlon <pauliescanlon@gmail.com>
2024-04-03 10:48:06 -0500

    feat: add portable text features

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'sanity';
+import { defineConfig, defineArrayMember } from 'sanity';
 import { structureTool } from 'sanity/structure';
 
 export default defineConfig({
@@ -28,6 +28,76 @@ export default defineConfig({
               source: 'title',
               maxLength: 96,
             },
+          },
+          {
+            title: 'Block Content',
+            name: 'blockContent',
+            type: 'array',
+            of: [
+              defineArrayMember({
+                title: 'Block',
+                type: 'block',
+                styles: [
+                  { title: 'H2', value: 'h2' },
+                  { title: 'H3', value: 'h3' },
+                  { title: 'H4', value: 'h4' },
+                  { title: 'Normal', value: 'p' },
+                  { title: 'Quote', value: 'blockquote' },
+                ],
+                lists: [
+                  { title: 'Bullet', value: 'bullet' },
+                  { title: 'Number', value: 'number' },
+                ],
+                marks: {
+                  decorators: [
+                    { title: 'Strong', value: 'strong' },
+                    { title: 'Emphasis', value: 'em' },
+                  ],
+                  annotations: [
+                    {
+                      title: 'URL',
+                      name: 'link',
+                      type: 'object',
+                      fields: [
+                        {
+                          title: 'URL',
+                          name: 'href',
+                          type: 'url',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              }),
+              {
+                title: 'Image',
+                type: 'image',
+                fields: [
+                  {
+                    name: 'alt',
+                    title: 'Alt',
+                    type: 'string',
+                  },
+                ],
+              },
+              {
+                title: 'CTA',
+                type: 'object',
+                name: 'cta',
+                fields: [
+                  {
+                    name: 'text',
+                    title: 'Text',
+                    type: 'string',
+                  },
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url',
+                  },
+                ],
+              },
+            ],
           },
         ],
       },
